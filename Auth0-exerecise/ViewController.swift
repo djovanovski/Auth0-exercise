e5f21beb4f8c50fb1d5c68583e87d8e45b4c3c81
djovanovski <djovanovski92@gmail.com>
2017-02-11 17:28:17 +0100

    initial commit

---
+++ Auth0-exerecise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auth0-exerecise
+//
+//  Created by Darko Jovanovski on 2/11/17.
+//  Copyright © 2017 Darko Jovanovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
